ff98b5d6f31ea2d890160d84fba965b6f625095e
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-09-17 02:55:37 +0300

    Delete src/components/api directory

--- src/components/api/Flights.tsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default async function handler(req, res){
-    const apiKey = process.env.AVIATIONSTACK_API_KEY;
-    const endpoint = process.env.AVIATION_URL
-}
