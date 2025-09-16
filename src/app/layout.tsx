91cd5df0b5e8ea7630cb36f99221d4aa6e784db8
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-09-17 02:51:40 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import News from "@/components/News";
-import Chatbot from "@/components/AplusAssistant";
 
 
 const geistSans = Geist({
