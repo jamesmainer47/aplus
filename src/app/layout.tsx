d550dcb0b16b88d6b8731a3f8a22333e25cb5aab
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-09-17 02:53:10 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         <News/>
-        <Chatbot/>
         {children}
         <Footer/>
       </body>
